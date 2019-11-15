9a4ef43a6cf2e6b515c0d277929ad1217b6f47f0
Piyush Garg <pgarg2598.ca18@chitkara.edu.in>
2019-11-16 00:43:29 +0530

    Security Fix

--- handlers/userHandler.js
+++ handlers/userHandler.js
@@ -1,7 +1,13 @@
 const User = require('../models/userModel')
 
 exports.signup = async(req,res,next) => {
-    const newUser = await User.create(req.body)
+    // const newUser = await User.create(req.body)
+    const newUser = await User.create({
+        name: req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        passwordConfirm:req.body.passwordConfirm
+    })
 
     res.status(201).json({
         status:'success',
